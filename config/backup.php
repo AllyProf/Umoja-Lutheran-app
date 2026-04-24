f408291b80b207e7cf3812979aa9e691d4cbc8b6
AllyProf <ally@umoja.com>
2026-04-24 14:27:08 +0300

    fix: Slugify backup name to avoid Google Drive path errors

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+        'name' => env('BACKUP_NAME', 'umoja-lutheran'),
 
         'source' => [
 
@@ -167,7 +167,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name' => env('APP_NAME', 'laravel-backup'),
+            'name' => env('BACKUP_NAME', 'umoja-lutheran'),
             'disks' => ['google'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
